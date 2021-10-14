60625e8e8427ad3206c16eeb15ef93d7f4d87123
Romilson Lemes <romilsonlemes@gmail.com>
2021-10-14 02:23:47 -0300

    Foi realizado uma impressao da ultima cotação do: Dolar Americano Euro e Bitcoin

--- cotarMoeda.py
+++ cotarMoeda.py
@@ -4,11 +4,12 @@ import json
 cotacoes = requests.get('https://economia.awesomeapi.com.br/json/all')
 cotacoes_dic = cotacoes.json()
 #print(cotacoes_dic['USD']['code'])
-print('Dolar Americano')
-print(cotacoes_dic['USD']) # Dolar Americano
-print('Dolar Canadense')
-print(cotacoes_dic['CAD']) # Dolar Canadense
-
+#print('Dolar Americano')
+#print(cotacoes_dic['USD'][bid]) # Dolar Americano, Campo BID
+print('\nValor da cotação para:\n')
+print(f'Dolar Americano: {cotacoes_dic["USD"]["bid"]}')
+print(f'Euro...........: {cotacoes_dic["EUR"]["bid"]}')
+print(f'BitCoin........: {cotacoes_dic["BTC"]["bid"]}')
 #print(cotacoes_dic)
 
 
