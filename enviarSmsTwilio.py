53c7e6eb3306148e74efa50cda4342f61612e5a8
Romilson Lemes <romilsonlemes@gmail.com>
2021-10-18 01:13:18 -0300

    Foi retiradas as chaves do Twilio do código fonte do programa de envio de mensagens.
     Devo terminar de configurar neste programa para que o fonte busque as chaves e o telefone de remetente do Twilio sejam encontrados no arquivo de configuração.

--- enviarSmsTwilio.py
+++ enviarSmsTwilio.py
@@ -1,16 +1,15 @@
 from twilio.rest import Client
 
-account_sid = 'ACec2c68efe231e3d99b8ca5333bc5b72c'
-auth_token = 'b0bc1a5ace100cca429ac1db1d7c32e3'
+account_sid = '' # Verificar esta informação no Arquivo de configuração deste Projeto
+auth_token = ''  # Verificar esta informação no Arquivo de configuração deste Projeto
 
 # Cliente de envio
 client = Client(account_sid, auth_token)
 
-remetente = '+15155178184'
+remetente = ''  # Verificar esta informação no Arquivo de configuração deste Projeto
 destino = '+5511981150868' #Romilson
-#destino = '+5516988196226' #Darlison
 
-sent_body="\n\nBoa tarde Senhor Engenheiro em Computação !!!"
+sent_body="\n\nBom dia Senhor Romilson Lemes !!!"
 
 message = client.messages.create(
     to=destino,
